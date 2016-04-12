1cc29804595c7f9409c52334019c183d90b5e033
kakalak-lumberJack <jwmannin@gmail.com>
2016-04-12 15:05:01 -0400

    bugfix

--- crafting.lua
+++ crafting.lua
@@ -18,7 +18,7 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-        output = "arabesque.sandstone_desert_stone_knot 4",
+        output = "arabesque:sandstone_desert_stone_knot 4",
         recipe = {
               {"default:desert_stone", "", "default:desert_stone"},
               {"", "default:sandstone", ""},
