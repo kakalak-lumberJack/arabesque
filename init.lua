40772b0ca6b0b78d81dc90d57cb96f6c27581ba9
kakalak-lumberJack <jwmannin@gmail.com>
2016-04-29 02:36:39 -0400

    Added files via upload

--- init.lua
+++ init.lua
@@ -22,7 +22,7 @@ minetest.register_node("arabesque:desert_stone_knot", {
                 fixed = {-0.5, -0.5, -0.5, 0.5, -0.44, 0.5}
                 },
         on_place = minetest.rotate_node,
-        groups = {cracky =2}
+        groups = {cracky =2},
         if is_corner == nil then
                 tiles = {
                 texture_path,
