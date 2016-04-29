dec44c45f6dc78ef76890fe7a433aa419dffc347
kakalak-lumberJack <jwmannin@gmail.com>
2016-04-29 02:27:53 -0400

    Added files via upload

--- init.lua
+++ init.lua
@@ -16,12 +16,11 @@ minetest.register_node("arabesque:lantern", {
 minetest.register_node("arabesque:desert_stone_knot", {
         description = "Desert Stone Knot Pattern",
         paramtype2 = "faceir",
-        drawtype= "nodebox",
+        drawtype = "nodebox",
         node_box = {
                 type = "fixed",
                 fixed = {-0.5, -0.5, -0.5, 0.5, -0.44, 0.5}
-                }
-        },
+                },
         on_place = minetest.rotate_node,
         groups = {cracky =2}
         if is_corner == nil then
@@ -53,7 +52,7 @@ minetest.register_node("arabesque:desert_stone_knot", {
                 }
         }
 
-      
+
 })
 
 local stair_nodes = {
